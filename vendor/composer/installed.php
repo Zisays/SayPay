c9bb10951ec647dd28aa1eb5c51a92e1dfd44cf5
Zisay <15593838@qq.com>
2022-05-31 09:43:21 +0800

    2022-05-13
    更新基础包

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '633d79b9e4be9f5d404c9a171dfbe24e8184884b',
+        'name' => 'zisay/saypay',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'zisay/saypay' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '633d79b9e4be9f5d404c9a171dfbe24e8184884b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
